4748a925ad20d3b774ae6b6b0330c414cbf39209
milklot <vbiostflex@gmail.com>
2021-04-27 20:48:13 -0400

    mvp

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,38 +1,55 @@
-/*
-  If `scheme_id` does not exist in the database:
+const db = require('../../data/db-config')
 
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
+const checkSchemeId = () => {
+  return async (req, res, next) => {
+    try {
+      const { scheme_id } = req.params
+      const scheme = await db("schemes").where( { scheme_id } ).first()
+      if (!scheme) {
+          return res.status(404).json({
+          message: `scheme with ID ${scheme_id} not found`,
+        })
+      }
+      req.scheme = scheme
+      next()
+    } catch(err) {
+      next(err)
+    }
   }
-*/
-const checkSchemeId = (req, res, next) => {
-
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
+const validateScheme = () => {
+  return async (req, res, next) => {
+    try {
+      const { scheme_name } = req.body;
 
-  status 400
-  {
-    "message": "invalid scheme_name"
+      if (!scheme_name || scheme_name === '' || typeof scheme_name !== 'string') {
+        await res.status(400).json({
+          message: `error with scheme name`,
+        })
+      }
+      next()
+    } catch(err) {
+      next(err)
+    }
   }
-*/
-const validateScheme = (req, res, next) => {
-
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
+const validateStep = () => {
+  return async (req, res, next) => {
+    try {
+      const { instructions, step_number } = req.body;
 
-  status 400
-  {
-    "message": "invalid step"
+      if (!instructions || instructions === '' || typeof instructions !== 'string' || typeof step_number !== 'number' || step_number < 1) {
+        await res.status(400).json({
+          message: `error with step`,
+        })
+      }
+      next();
+    } catch(err) {
+      next(err)
+    }
   }
-*/
-const validateStep = (req, res, next) => {
-
 }
 
 module.exports = {
